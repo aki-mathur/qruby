61c5f227007c191493a538c60a1547e27f8711f8
Akshat Mathur <mathurakshat8@gmail.com>
2020-04-03 15:03:19 +0530

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require_relative 'boot'
+
+# require 'rails/all'
+require "active_model/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "active_job/railtie" # Only for Rails >= 4.2
+require "action_cable/engine" # Only for Rails >= 5.0
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Qruby
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
