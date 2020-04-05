f1328f51f6709901eb0514853d670278b1af4cce
Akshat Mathur <mathurakshat8@gmail.com>
2020-04-05 16:45:43 +0530

    adder bower components to gitignore

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'qrcode#index'
-  get '/generate_code' => 'qrcode#generate_code'
-  post '/scan_code' => 'qrcode#scan_code'
-end
